f770540965034d99d8b72b9d45181e843dd1c7fc
Johannes Rappelt <johannes.rappelt@stud-mail.uni-wuerzburg.de>
2022-05-17 15:11:45 +0200

    small changes

--- src/main/kotlin/de/jagenka/DGSpawnManager.kt
+++ src/main/kotlin/de/jagenka/DGSpawnManager.kt
@@ -3,25 +3,27 @@ package de.jagenka
 import net.minecraft.server.network.ServerPlayerEntity
 import org.spongepowered.configurate.CommentedConfigurationNode
 
-object DGSpawnManager
+object DGSpawnManager //TODO: lobby spawn, spectator spawn, dead players spawn? -> maybe just their platform
 {
     private val spawns = ArrayList<Coords>()
-    private val teamSpawns = HashMap<DGTeam, Coords>()
+    private val teamSpawns = mutableMapOf<DGTeam?, Coords>().withDefault { defaultSpawn }
 
-    fun addSpawns(spawns: Collection<Coords>)
+    private var defaultSpawn = Coords(0.5, 80.0, 0.5, 0f, 0f)
+
+    private fun addSpawns(spawns: Collection<Coords>)
     {
         this.spawns.addAll(spawns)
     }
 
-    fun setSpawns(spawns: Collection<Coords>)
+    private fun setSpawns(spawns: Collection<Coords>)
     {
         this.spawns.clear()
         addSpawns(spawns)
     }
 
-    fun getSpawn(team: DGTeam) = teamSpawns[team]
+    fun getSpawn(team: DGTeam?) = teamSpawns.getValue(team)
 
-    fun getSpawn(player: ServerPlayerEntity) = DGPlayerManager.getTeam(player)?.let { getSpawn(it) }
+    fun getSpawn(player: ServerPlayerEntity) = getSpawn(DGPlayerManager.getTeam(player))
 
     fun shuffleSpawns()
     {
@@ -33,10 +35,13 @@ object DGSpawnManager
         val shuffledSpawns = spawns.shuffled()
         teamSpawns.clear()
         teams.forEachIndexed { index, team -> teamSpawns[team] = shuffledSpawns[index] }
+        //TODO: color spawn platforms
+        //TODO: print message if game is running, not at the beginning
     }
 
     fun loadConfig(root: CommentedConfigurationNode)
     {
         setSpawns(root.node("spawns").getList(Coords::class.java) ?: error("Error loading DeathGames spawns from config"))
+        defaultSpawn = root.node("defaultSpawn").get(Coords::class.java) ?: error("Error loading DeathGames defaultSpawn from config")
     }
 }
