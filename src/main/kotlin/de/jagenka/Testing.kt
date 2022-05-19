18a3354317bf86399cc559b9394327105142d8b7
Johannes Rappelt <mail@jayhannes.de>
2022-05-19 11:27:09 +0200

    clean Testing file - donut commit over it

--- src/main/kotlin/de/jagenka/Testing.kt
+++ src/main/kotlin/de/jagenka/Testing.kt
@@ -1,40 +1,10 @@
 package de.jagenka
 
-import de.jagenka.DGPlayerManager.clearTeam
-import de.jagenka.DGPlayerManager.registerToTeam
-import de.jagenka.Util.ifServerLoaded
-import de.jagenka.Util.teleport
-import net.minecraft.text.Text
-
 object Testing
 {
     @JvmStatic
     fun dropTest()
     {
-        println("drop it like it's hot")
 
-//        DGPlayerManager.getPlayer("HideoTurismo")?.let {
-//            it.registerToTeam(DGTeam.values().random())
-//            DGSpawnManager.shuffleSpawns()
-//            val spawn = DGSpawnManager.getSpawn(it)
-//            Util.sendChatMessage(spawn?.toString() ?: "no spawn assigned")
-//            ifServerLoaded { server ->
-//                if (spawn != null)
-//                {
-//                    val (x, y, z, yaw, pitch) = spawn
-//                    it.teleport(server.overworld, x, y, z, yaw, pitch)
-//                }
-//            }
-//        }
-//        Timer.toggle()
-
-//        DGPlayerManager.getPlayers().forEach { it.clearTeam() }
-//        DGPlayerManager.getPlayers().random().registerToTeam(DGTeam.random())
-//        DGSpawnManager.shuffleSpawns(DGPlayerManager.getNonEmptyTeams())
-//        DGPlayerManager.getPlayers().forEach { it.teleport(DGSpawnManager.getSpawn(it)) }
-
-        DGPlayerManager.getPlayers().forEach { it.registerToTeam(DGTeam.random()) }
-        DGPlayerManager.getPlayers().random().clearTeam()
-        DeathGames.startGame()
     }
 }
