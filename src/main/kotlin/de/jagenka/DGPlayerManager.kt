8575b592134eb0bfb0644fcf7039b9db2a3b9291
Johannes Rappelt <mail@jayhannes.de>
2022-05-23 18:30:17 +0200

    moved DGTeam to own file

--- src/main/kotlin/de/jagenka/DGPlayerManager.kt
+++ src/main/kotlin/de/jagenka/DGPlayerManager.kt
@@ -1,9 +1,6 @@
 package de.jagenka
 
 import de.jagenka.Util.ifServerLoaded
-import net.minecraft.block.Block
-import net.minecraft.block.BlockState
-import net.minecraft.block.Blocks
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.util.Formatting
 import net.minecraft.world.GameMode
@@ -137,41 +134,6 @@ data class DGPlayer(var playerEntity: ServerPlayerEntity, var inGame: Boolean)
     override fun hashCode(): Int
     {
         return playerEntity.hashCode()
-    }
-}
-
-enum class DGTeam
-{
-    BLACK, DARK_GREEN, DARK_AQUA, DARK_RED, DARK_PURPLE, GOLD, GRAY, DARK_GRAY, BLUE, GREEN, AQUA, RED, LIGHT_PURPLE, YELLOW;
-
-    fun getColoredBlock(): Block
-    {
-        return when (this)
-        {
-            BLACK -> Blocks.BLACK_CONCRETE
-            DARK_GREEN -> Blocks.GREEN_TERRACOTTA
-            DARK_AQUA -> Blocks.CYAN_CONCRETE
-            DARK_RED -> Blocks.RED_CONCRETE
-            DARK_PURPLE -> Blocks.PURPLE_CONCRETE
-            GOLD -> Blocks.ORANGE_CONCRETE
-            GRAY -> Blocks.LIGHT_GRAY_CONCRETE
-            DARK_GRAY -> Blocks.GRAY_CONCRETE
-            BLUE -> Blocks.BLUE_CONCRETE
-            GREEN -> Blocks.LIME_CONCRETE
-            AQUA -> Blocks.LIGHT_BLUE_CONCRETE
-            RED -> Blocks.RED_TERRACOTTA
-            LIGHT_PURPLE -> Blocks.MAGENTA_CONCRETE
-            YELLOW -> Blocks.YELLOW_CONCRETE
-        }
-    }
-
-    fun getPlayers() = DGPlayerManager.getPlayersInTeam(this)
-
-    fun getInGamePlayers() = DGPlayerManager.getInGamePlayersInTeam(this)
-
-    companion object
-    {
-        fun random() = values().random()
     }
 }
 
---
+++ src/main/kotlin/de/jagenka/DGTeam.kt
@@ -0,0 +1,39 @@
+package de.jagenka
+
+import net.minecraft.block.Block
+import net.minecraft.block.Blocks
+
+enum class DGTeam
+{
+    BLACK, DARK_GREEN, DARK_AQUA, DARK_RED, DARK_PURPLE, GOLD, GRAY, DARK_GRAY, BLUE, GREEN, AQUA, RED, LIGHT_PURPLE, YELLOW;
+
+    fun getColoredBlock(): Block
+    {
+        return when (this)
+        {
+            BLACK -> Blocks.BLACK_CONCRETE
+            DARK_GREEN -> Blocks.GREEN_TERRACOTTA
+            DARK_AQUA -> Blocks.CYAN_CONCRETE
+            DARK_RED -> Blocks.RED_CONCRETE
+            DARK_PURPLE -> Blocks.PURPLE_CONCRETE
+            GOLD -> Blocks.ORANGE_CONCRETE
+            GRAY -> Blocks.LIGHT_GRAY_CONCRETE
+            DARK_GRAY -> Blocks.GRAY_CONCRETE
+            BLUE -> Blocks.BLUE_CONCRETE
+            GREEN -> Blocks.LIME_CONCRETE
+            AQUA -> Blocks.LIGHT_BLUE_CONCRETE
+            RED -> Blocks.RED_TERRACOTTA
+            LIGHT_PURPLE -> Blocks.MAGENTA_CONCRETE
+            YELLOW -> Blocks.YELLOW_CONCRETE
+        }
+    }
+
+    fun getPlayers() = DGPlayerManager.getPlayersInTeam(this)
+
+    fun getInGamePlayers() = DGPlayerManager.getInGamePlayersInTeam(this)
+
+    companion object
+    {
+        fun random() = values().random()
+    }
+}
