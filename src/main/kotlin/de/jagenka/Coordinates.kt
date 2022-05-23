83fe19e2718353279b57ad6e52195ad5070786b4
Johannes Rappelt <mail@jayhannes.de>
2022-05-24 01:49:18 +0200

    moved extension funs

--- src/main/kotlin/de/jagenka/Coordinates.kt
+++ src/main/kotlin/de/jagenka/Coordinates.kt
@@ -11,8 +11,6 @@ data class Coordinates(val x: Double, val y: Double, val z: Double, val yaw: Flo
     constructor(pos: Vec3d) : this(pos.x, pos.y, pos.z)
     constructor(x: Int, y: Int, z: Int) : this(x.toDouble(), y.toDouble(), z.toDouble())
 
-    operator fun Coordinates.plus(other: Coordinates) = Coordinates(this.x + other.x, this.y + other.y, this.z + other.z, this.yaw, this.pitch)
-    operator fun Coordinates.minus(other: Coordinates) = Coordinates(this.x - other.x, this.y - other.y, this.z - other.z, this.yaw, this.pitch)
     infix fun distanceTo(other: Coordinates) = (other - this).length()
     private fun length() = sqrt(this.x.pow(2) + this.y.pow(2) + this.z.pow(2))
 
@@ -21,5 +19,9 @@ data class Coordinates(val x: Double, val y: Double, val z: Double, val yaw: Flo
 
     fun toVec3d() = Vec3d(x, y, z)
 }
+
+operator fun Coordinates.plus(other: Coordinates) = Coordinates(this.x + other.x, this.y + other.y, this.z + other.z, this.yaw, this.pitch)
+operator fun Coordinates.minus(other: Coordinates) = Coordinates(this.x - other.x, this.y - other.y, this.z - other.z, this.yaw, this.pitch)
+
 
 fun Vec3d.toDGCoordinates() = Coordinates(this)
