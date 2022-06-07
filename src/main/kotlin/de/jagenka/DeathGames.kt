52b95431824f8414b7fd0b12dc96a51150da88b4
s361591 <gino.schulze-skyfire@web.de>
2022-06-07 22:02:56 +0200

    Implemented Game start/Game over message.

--- src/main/kotlin/de/jagenka/DeathGames.kt
+++ src/main/kotlin/de/jagenka/DeathGames.kt
@@ -10,8 +10,14 @@ import de.jagenka.managers.PlayerManager.makeInGame
 import de.jagenka.managers.SpawnManager.getSpawn
 import de.jagenka.shop.Shop
 import de.jagenka.timer.Timer
+import de.jagenka.timer.seconds
 import net.fabricmc.api.DedicatedServerModInitializer
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback
+import net.minecraft.text.LiteralText
+import net.minecraft.text.Style
+import net.minecraft.text.Text
+import net.minecraft.text.Texts
+import net.minecraft.util.Formatting
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.GameMode
 
@@ -77,6 +83,9 @@ object DeathGames : DedicatedServerModInitializer
         //TODO: remove arrows / items from map
 
         //TODO: show begin message
+        PlayerManager.getOnlinePlayers().forEach { player ->
+            DisplayManager.sendTitleMessage(player, Text.of("Whaddup fuckers"), Text.of("Ready to fuck?"), 5.seconds())
+        }
 
         Timer.start()
         running = true
@@ -84,7 +93,17 @@ object DeathGames : DedicatedServerModInitializer
 
     fun stopGame()
     {
-        DisplayManager.sendChatMessage("GAME OVER")
+        val winners = mutableListOf<Text>()
+        PlayerManager.getOnlineInGameTeams().forEach { team ->
+            winners.add(LiteralText("$team").getWithStyle(Style.EMPTY.withColor(Formatting.byName(team.name.lowercase())))[0])
+        }
+        val winnerPlayers = Texts.join(winners, Text.of(", "))
+        winners.clear()
+        if (winners.count() < 2) winners.add(Text.of("Winner")) else winners.add(Text.of("Winners"))
+        winners.add(winnerPlayers)
+        PlayerManager.getOnlinePlayers().forEach {
+            DisplayManager.sendTitleMessage(it, Text.of("Game Over"), Texts.join(winners, Text.of(": ")), 5.seconds())
+        }
         running = false
         PlayerManager.getOnlinePlayers().forEach { it.changeGameMode(GameMode.SPECTATOR) }
         //TODO: timer, back to lobby, etc
