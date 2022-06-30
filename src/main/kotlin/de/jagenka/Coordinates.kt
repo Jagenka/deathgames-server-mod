4e19088b502888a7a3117389189e060a113269ff
Johannes Rappelt <mail@jayhannes.de>
2022-06-30 16:56:15 +0200

    WIP

--- src/main/kotlin/de/jagenka/Coordinates.kt
+++ src/main/kotlin/de/jagenka/Coordinates.kt
@@ -9,15 +9,15 @@ import kotlin.math.sqrt
 @Serializable
 data class Coordinates(val x: Int, val y: Int, val z: Int, val yaw: Float, val pitch: Float)
 {
-    constructor(pos: Vec3d) : this(pos.x, pos.y, pos.z)
-    constructor(x: Double, y: Double, z: Double, yaw: Float = 0f, pitch: Float = 0f) : this(x.floor(), y.floor(), z.floor(), yaw, pitch)
+    constructor(pos: Vec3d, yaw: Float, pitch: Float) : this(pos.x, pos.y, pos.z, yaw, pitch)
+    constructor(x: Double, y: Double, z: Double, yaw: Float, pitch: Float) : this(x.floor(), y.floor(), z.floor(), yaw, pitch)
 
     @Deprecated("obsolete since Coordinates has Int xyz", level = DeprecationLevel.WARNING)
     infix fun distanceTo(other: Coordinates) = (other - this).length()
     infix fun distanceTo(pos: Vec3d) = sqrt((this.x.toCenter() - pos.x).pow(2) + (this.y - pos.y).pow(2) + (this.z.toCenter() - pos.z).pow(2))
     private fun length() = sqrt(this.x.toDouble().pow(2) + this.y.toDouble().pow(2) + this.z.toDouble().pow(2))
 
-    fun relative(x: Double, y: Double, z: Double) = Coordinates(this.x + x, this.y + y, this.z + z)
+    fun relative(x: Double, y: Double, z: Double) = Coordinates(this.x + x, this.y + y, this.z + z, this.yaw, this.pitch)
     fun relative(x: Int, y: Int, z: Int) = relative(x.toDouble(), y.toDouble(), z.toDouble())
 
     fun toVec3d() = Vec3d(x.toCenter(), y.toDouble(), z.toCenter())
@@ -35,5 +35,5 @@ operator fun Coordinates.times(value: Double) = Coordinates((this.x * value).flo
 operator fun Coordinates.div(value: Double) = Coordinates((this.x / value).floor(), (this.y / value).floor(), (this.z / value).floor(), this.yaw, this.pitch)
 
 
-fun Vec3d.toDGCoordinates() = Coordinates(this)
+//fun Vec3d.toDGCoordinates() = Coordinates(this)
 fun ServerPlayerEntity.toDGCoordinates() = Coordinates(this.pos.x, this.pos.y, this.pos.z, this.yaw, this.pitch)
