3828c4f18fcd69fc05fd967f39097071215fbb21
Runebreaker <gino.schulze-skyfire@web.de>
2023-02-22 22:56:10 +0100

    Some more extension functions for Vec3d

--- src/main/kotlin/de/jagenka/Util.kt
+++ src/main/kotlin/de/jagenka/Util.kt
@@ -242,6 +242,8 @@ fun Float.toRadians(): Float = (this / 180f) * Math.PI.toFloat()
 
 fun Float.toDegree(): Float = (this * 180f) / Math.PI.toFloat()
 
+operator fun Vec3d.plus(other: Vec3d): Vec3d = this.add(other)
+
 operator fun Vec3d.minus(other: Vec3d): Vec3d = this.subtract(other)
 
 operator fun Vec3d.times(factor: Double): Vec3d = this.multiply(factor)
