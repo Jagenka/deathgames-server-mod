c7e89c4ccf41be44b545dbf44731eec666cfb93a
Jagenka <dev@jayhannes.de>
2024-10-06 21:27:33 +0200

    add TODO

--- src/main/kotlin/de/jagenka/Testing.kt
+++ src/main/kotlin/de/jagenka/Testing.kt
@@ -12,4 +12,5 @@ object Testing
 /*
     TODO: sound when popping trap -- should work?? investigate alternative method call
     TODO: rename nbt to components in config
+    TODO: edit config for rage map
  */
