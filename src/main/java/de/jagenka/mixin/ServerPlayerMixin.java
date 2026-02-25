8d083b9f009fa0fe55dfd147aa1a10587a86e96e
Jagenka <dev@jayhannes.de>
2026-02-25 20:34:35 +0100

    fix prevent drop

--- src/main/java/de/jagenka/mixin/ServerPlayerMixin.java
+++ src/main/java/de/jagenka/mixin/ServerPlayerMixin.java
@@ -7,6 +7,8 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.stats.Stat;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -37,13 +39,21 @@ public class ServerPlayerMixin
         StatManager.handleDeathType(damageSource, ((ServerPlayer) (Object) this).getName().getString());
     }
 
-    @Inject(method = "drop*", at = @At("HEAD"), cancellable = true)
-    private void preventDrop(CallbackInfoReturnable<Boolean> cir)
+    @Inject(method = "drop(Lnet/minecraft/world/item/ItemStack;ZZ)Lnet/minecraft/world/entity/item/ItemEntity;", at = @At("HEAD"), cancellable = true)
+    private void preventDrop(ItemStack itemStack, boolean bl, boolean bl2, CallbackInfoReturnable<ItemEntity> cir)
     {
         if (!Config.INSTANCE.isEnabled()) return;
 
-        cir.setReturnValue(false);
+        cir.setReturnValue(null);
         cir.cancel();
+        ((ServerPlayer) (Object) this).inventoryMenu.sendAllDataToRemote();
+    }
+
+    @Inject(method = "drop(Z)V", at = @At("HEAD"), cancellable = true)
+    private void preventDrop(boolean bl, CallbackInfo ci) {
+        if (!Config.INSTANCE.isEnabled()) return;
+
+        ci.cancel();
         ((ServerPlayer) (Object) this).inventoryMenu.sendAllDataToRemote();
     }
 
